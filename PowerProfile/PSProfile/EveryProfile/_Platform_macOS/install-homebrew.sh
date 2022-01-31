1eca34b9ce05a5554fa8891b5c33a9af64916db1
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2022-01-31 17:01:35 +0100

    add command line tools install

--- PowerProfile/PSProfile/EveryProfile/_Platform_macOS/install-homebrew.sh
+++ PowerProfile/PSProfile/EveryProfile/_Platform_macOS/install-homebrew.sh
@@ -2,6 +2,9 @@
 
 if [ -z "`command -v brew`" ]; then
     if [[ "$(read -e -p '      Install Homebrew now? [y/N]> '; echo $REPLY)" == [YyZz]* ]]; then
+        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+        softwareupdate -i -a
+        rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
         /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 fi
