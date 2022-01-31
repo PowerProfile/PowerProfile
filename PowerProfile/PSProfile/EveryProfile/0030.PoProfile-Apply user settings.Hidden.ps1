3623788dcd8014986689c74b34f439ba0c4c5205
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2022-01-31 17:00:51 +0100

    Add Environment variable and Binary application

--- PowerProfile/PSProfile/EveryProfile/0030.PoProfile-Apply user settings.Hidden.ps1
+++ PowerProfile/PSProfile/EveryProfile/0030.PoProfile-Apply user settings.Hidden.ps1
@@ -15,47 +15,79 @@ foreach ($File in $Settings.GetEnumerator()) {
         continue
     }
 
-    if ($null -eq $Cfg.Commands) {
-        continue
-    }
+    if ($null -ne $Cfg.ModuleName) {
+        if (-Not (Get-Module -ListAvailable -Name $Cfg.ModuleName)) {
+            continue
+        }
 
-    elseif (
-        $null -ne $Cfg.ModuleName -and
-        (
+        if (
             (
                 $null -ne $Cfg.MinimumVersion -and
-                ((Get-Module $Cfg.ModuleName).Version -lt [version]$Cfg.MinimumVersion)
+                ((Get-Module -ListAvailable -Name $Cfg.ModuleName).Version -lt [version]$Cfg.MinimumVersion)
             ) -or
             (
                 $null -ne $Cfg.MaximumVersion -and
-                ((Get-Module $Cfg.ModuleName).Version -gt [version]$Cfg.MaximumVersion)
+                ((Get-Module -ListAvailable -Name $Cfg.ModuleName).Version -gt [version]$Cfg.MaximumVersion)
             ) -or
             (
                 $null -ne $Cfg.RequiredVersion -and
-                ((Get-Module $Cfg.ModuleName).Version -ne [version]$Cfg.RequiredVersion)
+                ((Get-Module -ListAvailable -Name $Cfg.ModuleName).Version -ne [version]$Cfg.RequiredVersion)
             )
-        )
-    ) {
-        Continue
+        ) {
+            Continue
+        }
     }
 
-    foreach ($Command in $Cfg.Commands.GetEnumerator()) {
-        if ($Command.Value -is [array]) {
-            foreach ($Params in $Command.Value) {
+    if ($null -ne $Cfg.ApplicationName) {
+        if (-Not (Get-Command -CommandType Application -Name $Cfg.ApplicationName)) {
+            continue
+        }
+    }
+
+    if ($null -ne $Cfg.EnvironmentVariables) {
+        foreach ($EnvVar in $Cfg.EnvironmentVariables.GetEnumerator()) {
+            try {
+                [System.Environment]::SetEnvironmentVariable($EnvVar.Name,$EnvVar.Value)
+            }
+            catch {
+
+            }
+        }
+    }
+
+    if ($null -ne $Cfg.Commands) {
+        foreach ($Command in $Cfg.Commands.GetEnumerator()) {
+            if ($Command.Value -is [array]) {
+                foreach ($Params in $Command.Value) {
+
+                    # repeat the same PowerShell command using different parameters
+                    if ($Params -is [Hashtable]) {
+                        try {
+                            Invoke-Expression "$($Command.Name) @Params"
+                        }
+                        catch {
+
+                        }
+                    }
+
+                    # One or many commands without parameter hash
+                    else {
+                        try {
+                            Invoke-Expression "$($Params)"
+                        }
+                        catch {
+
+                        }
+                    }
+                }
+            } elseif ($Command.Value -is [Hashtable]) {
+                $Params = $Command.Value
                 try {
                     Invoke-Expression "$($Command.Name) @Params"
                 }
                 catch {
 
                 }
-            }
-        } elseif ($Command.Value -is [Hashtable]) {
-            $Params = $Command.Value
-            try {
-                Invoke-Expression "$($Command.Name) @Params"
-            }
-            catch {
-
             }
         }
     }
